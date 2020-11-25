6b0440cf688c3aed55e0c844aa22dc991fe947af
Nitsan Avni <nitsan@papayaglobal.com>
2020-11-25 10:14:02 +0200

    add mutation test

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
 import { upperCase } from "lodash";
 
-export const text = upperCase("hello there!");
+export const text = (() => upperCase("hello there!"))();
+export const notTested = "where's my test?";
--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,6 @@ import { text } from "../src/lib";
 
 describe("the source", () => {
     it("should pass", () => {
-        expect(text).toEqual("hello");
+        expect(text).toEqual(jasmine.stringMatching(/hello/i));
     });
 });
